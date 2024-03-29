0ed2f5436573ee1e549e9872881383aad08e36d2
teguhpermadi <tguh.permadi@gmail.com>
2024-03-29 17:38:36 +0700

    navigation badge

--- app/Filament/Resources/ProjectCoordinatorResource.php
+++ app/Filament/Resources/ProjectCoordinatorResource.php
@@ -29,6 +29,11 @@ class ProjectCoordinatorResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
     public static function form(Form $form): Form
     {
         return $form
--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -26,6 +26,11 @@ class UserResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
     public static function form(Form $form): Form
     {
         return $form
